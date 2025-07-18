f3a909ae655da0a0955e119b456656b975e34cdf
lucasmelloec <lucasmelloec@gmail.com>
2025-07-18 12:05:32 -0300

    add nix flakes

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            python3
+            mopidy
+            gst_all_1.gstreamer
+            gst_all_1.gst-plugins-bad
+            gst_all_1.gst-plugins-base
+            gst_all_1.gst-plugins-good
+            gst_all_1.gst-plugins-ugly
+            gst_all_1.gst-plugins-rs
+          ];
+
+          shellHook = ''
+            if test ! -d .venv; then
+              python3 -m venv .venv
+            fi
+            source ./.venv/bin/activate
+            export PYTHONPATH=`pwd`/$VENV/${pkgs.python3.sitePackages}/:$PYTHONPATH
+            pip install -r requirements.txt
+          '';
+        };
+      }
+    );
+}
